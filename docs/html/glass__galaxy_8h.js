cf5b49af5c779b5ac4bc3401facaa92968860650
Tyson Littenberg <tyson.b.littenberg@gmail.com>
2025-06-03 10:28:24 -0500

    adding some new docs and bulid files

---
+++ docs/html/glass__galaxy_8h.js
@@ -0,0 +1,11 @@
+var glass__galaxy_8h =
+[
+    [ "GalaxyModulation", "structGalaxyModulation.html", "structGalaxyModulation" ],
+    [ "GALAXY_A", "glass__galaxy_8h.html#af1732e437f339711f74f2f7df44ea3dd", null ],
+    [ "GALAXY_Rb", "glass__galaxy_8h.html#afac874947e84825ef130ea854024b05e", null ],
+    [ "GALAXY_Rd", "glass__galaxy_8h.html#a2ccd878fe3a37e004f934bc04dcc929f", null ],
+    [ "GALAXY_RGC", "glass__galaxy_8h.html#aea898e46a8da1ee725d9d8206432d187", null ],
+    [ "GALAXY_Zd", "glass__galaxy_8h.html#abe95fd02e34e2d13fd65c84dbdb7b6cc", null ],
+    [ "LMAX", "glass__galaxy_8h.html#a5b086514f6963b35ee29bc954fc9b0f3", null ],
+    [ "NSIDE", "glass__galaxy_8h.html#a3d77c69326672a5b98ff804d033846f1", null ]
+];
---
+++ docs/html/structGalaxyModulation.js
@@ -0,0 +1,14 @@
+var structGalaxyModulation =
+[
+    [ "N", "structGalaxyModulation.html#a9669fc01698e6da0c7c58202aac4f60b", null ],
+    [ "Npix", "structGalaxyModulation.html#a367cd3e39bf5063e9b3bb2823da7e186", null ],
+    [ "skyphi", "structGalaxyModulation.html#ab25e08bf36ccebccf1072d7dad5d88cb", null ],
+    [ "skytheta", "structGalaxyModulation.html#a4db79c52f355b3621c6962d8b1ad3a99", null ],
+    [ "t", "structGalaxyModulation.html#ae62a055e7a1a9e862f47ef5761239382", null ],
+    [ "XX_spline", "structGalaxyModulation.html#a09002c11b69813c8350beb89f2e8d77e", null ],
+    [ "XY_spline", "structGalaxyModulation.html#a8509b7e880ec3ce9b2d22d8d58de20d1", null ],
+    [ "XZ_spline", "structGalaxyModulation.html#af7b17d6d6712526484fcee25d381247e", null ],
+    [ "YY_spline", "structGalaxyModulation.html#a88aebb69175895ff7f006bcb91052f95", null ],
+    [ "YZ_spline", "structGalaxyModulation.html#a077904f85b9611e0d7dd80ff22d16450", null ],
+    [ "ZZ_spline", "structGalaxyModulation.html#a41bf8acdba9e53361196169d6db5808c", null ]
+];
